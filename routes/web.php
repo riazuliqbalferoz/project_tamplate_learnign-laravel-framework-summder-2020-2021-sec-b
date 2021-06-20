0087bf7033888a8dfc2ea5472114c5576fa39a23
NafisianCastle <nafis.cseaiub@gmail.com>
2021-06-20 20:43:16 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\register_controller;
 |
 */
 
-Route::get('/', [index_controller::class, 'index'])->name('index');
+Route::get('/', [index_controller::class, 'index']);
 Route::get('/login', [login_controller::class, 'index'])->name('login');
 Route::get('/dashboard', [dashoard_controller::class, 'index'])->name('dashboard');
 Route::get('/register', [register_controller::class, 'index'])->name('register');
