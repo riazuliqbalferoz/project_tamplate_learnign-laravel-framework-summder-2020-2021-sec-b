8b7122f644e51538df2f2c025e8f85b04573fb7d
NafisianCastle <nafis.cseaiub@gmail.com>
2021-06-20 20:54:13 +0600

    freshly created project

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\index_controller;
-use App\Http\Controllers\login_controller;
-use App\Http\Controllers\dashoard_controller;
-use App\Http\Controllers\register_controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +13,6 @@ use App\Http\Controllers\register_controller;
 |
 */
 
-Route::get('/', [index_controller::class, 'index']);
-Route::get('/login', [login_controller::class, 'index'])->name('login');
-Route::get('/dashboard', [dashoard_controller::class, 'index'])->name('dashboard');
-Route::get('/register', [register_controller::class, 'index'])->name('register');
+Route::get('/', function () {
+    return view('welcome');
+});
