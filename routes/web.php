3c1be8a1134f147cea6f2720d0f3b3ed20ca712d
NafisianCastle <nafis.cseaiub@gmail.com>
2021-06-23 10:33:47 +0600

    controller added

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,10 @@ use App\Http\Controllers\index_controller;
 use App\Http\Controllers\login_controller;
 use App\Http\Controllers\dashoard_controller;
 use App\Http\Controllers\register_controller;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CheckoutController;
 use PhpParser\Node\Stmt\Return_;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [index_controller::class, 'index'])->name('index');
 Route::get('/login', [login_controller::class, 'index'])->name('login');
@@ -26,18 +18,11 @@ Route::get('/register', [register_controller::class, 'index'])->name('register')
 Route::get('/shop', function () {
     return view('shop');
 });
-Route::get('/cart', function () {
-    return view('cart');
-});
-Route::get('/checkout', function () {
-    return view('checkout');
-});
-Route::get('/homes', function () {
-    return view('home');
-});
+Route::get('/cart',[CartController::class,'index']);
+Route::get('/checkout',[CheckoutController::class,'index']);
+Route::get('/detail',[DetailController::class,'index']);
+Route::get('/shop',[ShopCartController::class,'index']);
 Route::get('/thankyou', function () {
     return view('thankyou');
 });
-Route::get('/detail', function () {
-    return view('detail');
-});
+
