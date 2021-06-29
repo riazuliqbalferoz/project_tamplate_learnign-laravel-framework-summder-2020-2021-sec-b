0727743dfee8ee802bf9814056193b513c0bd204
NafisianCastle <nafis.cseaiub@gmail.com>
2021-06-30 01:40:51 +0600

    user login, header, footer added

--- app/Http/Controllers/index_controller.php
+++ app/Http/Controllers/index_controller.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class index_controller extends Controller
 {
+
     public function index()
     {
         return view('start');
--- app/Http/Controllers/login_controller.php
+++ app/Http/Controllers/login_controller.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class login_controller extends Controller
 {
-
     public function index()
     {
         return view('login');
