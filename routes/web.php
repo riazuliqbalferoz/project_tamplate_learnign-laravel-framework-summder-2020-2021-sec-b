1ac24b4e96efc03b976fee13bc84240222486d73
NafisianCastle <nafis.cseaiub@gmail.com>
2021-06-30 04:42:07 +0600

    login done

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\index_controller;
 use App\Http\Controllers\login_controller;
 use App\Http\Controllers\dashoard_controller;
 use App\Http\Controllers\register_controller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 use PhpParser\Node\Stmt\Return_;
@@ -14,6 +16,7 @@ Route::get('/login', [login_controller::class, 'index'])->name('login');
 Route::get('/dashboard', [dashoard_controller::class, 'index'])->name('dashboard');
 Route::get('/register', [register_controller::class, 'index'])->name('register');
 
-Route::get('/ecommerce.com', function () {
-    return view('userLogin');
-});
+Route::get('/userlogin',[UserController::class,'index'] );
+Route::post('/userlogin',[UserController::class,'login'] );
+Route::get('/product',[ProductController::class,'index'] );
+
