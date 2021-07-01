5648a84f080680a10254f16026a10b627c01a466
NafisianCastle <nafis.cseaiub@gmail.com>
2021-07-01 19:53:14 +0600

    cart list crated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
@@ -41,5 +41,16 @@ class ProductController extends Controller
     {
         $userId =Session::get('user')['id'];
         return Cart::where('user_id',$userId)->count();
+    }
+    function cartList()
+    {
+        $userId =Session::get('user')['id'];
+        $products = DB::table('cart')
+        ->join('products','cart.product_id','=','products.id' )
+        ->where('cart.user_id', $userId)
+        ->select('products.*')
+        ->get();
+
+        return view('cartlist',['products'=>$products]);
     }
 }
